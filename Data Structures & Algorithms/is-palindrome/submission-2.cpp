d3fb0cca6bb0e7a84672768b51852bea580e5450
shobhna04 <btech11106.25@bitmesra.ac.in>
2026-08-25 12:05:51 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<=right)
+        {
+              if (!isalnum(s[left]) ) {
+                
+                left++;
+        continue; }
+            if (!isalnum(s[right]) ) {
+                
+                right--;
+        continue; }
+         else if(tolower(s[left])!=tolower(s[right]))
+            {
+        return false;
+            }
+
+      right--;
+      left++;
+          
+        }
+            
+            return true;
+         
+    }
+};
